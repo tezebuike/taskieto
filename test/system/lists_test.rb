ea0548f3ff2f4977b378e32f1677e2950e3a5887
tezebuike <tracy.ezebuike@andela.com>
2019-07-09 10:28:10 +0100

    remove view spec

--- test/system/lists_test.rb
+++
@@ -1,9 +0,0 @@
-require "application_system_test_case"
-
-class ListsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit lists_url
-  #
-  #   assert_selector "h1", text: "List"
-  # end
-end
